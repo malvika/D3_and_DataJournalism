497f72c8df5983d49ac57dd3aa5298534875b664
Malvika <malvika.mathur@Gmail.com>
2018-07-23 23:30:15 -0400

    fixing the graph

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart,
-//and shift the latter by left and top margins.
+// and shift the latter by left and top margins.
 var svg = d3
 	.select('.chart')
 	.append("svg")
@@ -21,35 +21,33 @@ var svg = d3
 	.attr("height", svgHeight)
 	.append("g")
 
-
-  // Append group element
-  var chart = svg.append("g")
+// Append group element
+	var chart = svg.append("g")
 	.attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-d3.select("body")
-	.append("div")
-	.attr("class", "tooltip")
-	.style("opacity", 1)
-	
-  // Import data from the data.csv file, call the function healthData
+  d3.select("body")
+	 .append("div")
+	 .attr("class", "tooltip")
+	 .style("opacity", 1)
+
+// Import data from the data.csv file, call the function healthData
   d3.csv("data.csv", function(error, healthData) {
 	if (error) throw error;
 
-
-   // Parse data
+ // Parse data
   healthData.forEach(function(data) {
 	  data.poverty = +data.poverty;
 	  data.healthStatus = +data.healthStatus;
   });
 
-	// Create the scales for the chart
+// Create the scales for the chart
 	var x = d3.scaleLinear().range([0, chartWidth]);
 	var y = d3.scaleLinear().range([chartHeight, 0]);
 
 	var bottomAxis = d3.axisBottom(x);
 	var leftAxis = d3.axisLeft(y);
 
-	//  Scale the range of the data
+// Scale the range of the data
 	x.domain([0,d3.max(healthData, function(data){
 		return +data.poverty;
 	})]);
@@ -58,7 +56,7 @@ d3.select("body")
 		return +data.healthStatus;
 	})]);
 
-	// Defining tooltip
+// Defining tooltip
 	var toolTip = d3.tip()
 		.attr("class", "toolTip")
 		.offset([80,-60])
@@ -66,12 +64,12 @@ d3.select("body")
 			var state = data.state;
 			var povertyRate = +data.poverty;
 			var healthStatus = +data.healthStatus;
-			return(state + "<br> Poverty Rate: " + povertyRate + "<br> Percentage of the population in fair or poor health: " + healthStatus)
+			return(state + "<br> Poverty Rate (%): " + povertyRate + "<br> Health Condition (%): " + healthStatus)
 		});
 
 	chart.call(toolTip);
 
-  // Defining the circles on the chart 
+// Defining the circles on the chart
 	chart.selectAll("circle")
 		.data(healthData)
 		.enter().append("circle")
@@ -85,34 +83,40 @@ d3.select("body")
 			})
 			.attr('r', "10")
 			.attr("fill", "blue")
-			.style("opacity", .5)
+			.style("opacity", 0.5)
 			.on("click", function(data){
 				toolTip.show(data);
 			});
 
-	// Add the x-axis
+// Add the x-axis
 	chart.append("g")
 		.attr("transform", `translate(0, ${chartHeight})`)
 		.call(bottomAxis);
-	
-	// Add the y-axis
+
+// Add the y-axis
 	chart.append('g')
 		.call(leftAxis);
 
-    // Text for the y-axis
+// Text for the y-axis
 	chart.append("text")
 		.attr("transform", "rotate(-90)")
 		.attr("y", 0 - margin.left + 40)
 		.attr("x", 0 - (chartHeight))
 		.attr("dy", "1em")
 		.attr("class", "axisText")
-		.style("text-anchor", "top")
+		.style("text-anchor", "margintop")
 		.text("Population in Fair or Poor Health (%)")
 
-   // Text for the x-axis
+// Text for the x-axis
 	chart.append("text")
 		.attr("transform", "translate(" + (chartWidth/2) + ", " + (chartHeight + margin.top + 20) + ")")
 		.attr("class", "axisText")
 		.style("text-anchor", "middle")
 		.text("Population Below the Poverty Line (%)");
+
+// Text for title
+chart.append("text")
+	.style("text-anchor", "right")
+	.attr("class", "axisText")
+	.text("Correlation of Health vs. Poverty in USA");
 })
